a6cebce23ab893003efcc175c7b980679f783476
Cory Shain <cory.shain@gmail.com>
2018-04-14 21:49:32 -0400

    added support for CV data and tensorboard logging

--- semprocDNN/data.py
+++ semprocDNN/data.py
@@ -10,22 +10,21 @@ class Data(object):
         n = {}
         with open(path, 'r') as file:
             for line in file:
-                decpar = self.parse_decpar(line)
-                for ID in decpar:
-                    if ID not in n:
-                        n[ID] = 1
+                ID, condition, consequent, count = self.parse_decpar(line)
+                if ID not in n:
+                    n[ID] = 1
+                else:
+                    n[ID] += 1
+                if ID not in decpars:
+                    decpars[ID] = {condition: {consequent: count}}
+                else:
+                    if condition not in decpars[ID]:
+                        decpars[ID][condition] = {consequent: count}
                     else:
-                        n[ID] += 1
-                    if ID not in decpars:
-                        decpars[ID] = decpar[ID]
-                    else:
-                        for condition in decpar[ID]:
-                            if condition not in decpars[ID]:
-                                decpars[ID][condition] = decpar[ID][condition]
-                            else:
-                                for consequent in decpar[ID][condition]:
-                                    if consequent not in decpars[ID][condition]:
-                                        decpars[ID][condition][consequent] = float(decpar[ID][condition][consequent])
+                        if consequent not in decpars[ID][condition]:
+                            decpars[ID][condition][consequent] = float(count)
+                        else:
+                            decpars[ID][condition][consequent] += float(count)
                 if verbose and i > 0 and i%100000 == 0:
                     sys.stderr.write('%s lines processed\n' %i)
                 i += 1
@@ -44,11 +43,44 @@ class Data(object):
         consequent = tokens[3]
         count = float(tokens[5])
 
-        decpar = {model_id: {condition: {consequent: count}}}
+        decpar = [model_id, condition, consequent, count]
 
         return decpar
 
+    def encode_data(self, path, ID):
+        X = [None, [[], []]]
+        y = []
+        w = []
 
+        with open(path, 'r') as file:
+            i = 0
+            for line in file:
+                ID_dp, condition, consequent, count = self.parse_decpar(line)
+                if ID_dp == ID:
+                    for sub_condition in condition:
+                        if sub_condition in getattr(self, ID).condition2ix:
+                            cond_ix = getattr(self, ID).condition2ix[sub_condition]
+                            X[1][0].append(i)
+                            X[1][1].append(cond_ix)
+                    y.append(getattr(self, ID).consequent2ix.get(consequent, len(getattr(self, ID).ix2consequent)))
+                    w.append(float(count))
+                i += 1
+
+        X[0] = np.ones((len(X[1][0]),), dtype='uint8')
+        assert len(X[0]) == len(X[1][0]) == len(X[1][1])
+
+        X[1][0] = np.array(X[1][0], dtype='int64')
+        X[1][1] = np.array(X[1][1], dtype='int64')
+        X[1] = tuple(X[1])
+        X = tuple(X)
+
+        X = coo_matrix(X, (i, getattr(self, ID).n_condition))
+        X = X.tocsr()
+
+        y = np.array(y, dtype='int32')
+        w = np.array(w, dtype='float32')
+
+        return X, y, w
 
 
 class Model(object):
@@ -109,5 +141,5 @@ class Model(object):
         X = X.tocsr()
 
         return X, y, w
-    
+
 
--- semprocDNN/main.py
+++ semprocDNN/main.py
@@ -4,16 +4,31 @@ from data import Data
 from semproc_io import dump_model
 import argparse
 import numpy as np
-import pandas as pd
 import os
 
 
 
-def dump_embeddings():
-    dump_model(embedding_weights, 'embedding_weights.txt', 'E', d.F.ix2condition, range(1,embedding_dim+1), session=sess)
+def dump_embeddings(path, embdim):
+    dump_model(embedding_weights, path + '/embedding_weights.txt', 'E', d.F.ix2condition, range(embdim), session=sess)
 
-def dump_weights():
-    dump_model(weights_final, 'weights.txt', 'F', range(1,embedding_dim+1), d.F.ix2consequent, session=sess)
+def dump_weights(path, embdim):
+    dump_model(weights_final, path + '/weights.txt', 'F', range(embdim), d.F.ix2consequent, session=sess)
+
+def report_n_params(sess):
+    with sess.as_default():
+        with sess.graph.as_default():
+            n_params = 0
+            var_names = [v.name for v in tf.trainable_variables()]
+            var_vals = sess.run(tf.trainable_variables())
+            sys.stderr.write('Trainable variables:\n')
+            for i in range(len(var_names)):
+                v_name = var_names[i]
+                v_val = var_vals[i]
+                cur_params = np.prod(np.array(v_val).shape)
+                n_params += cur_params
+                sys.stderr.write('  ' + v_name.split(':')[0] + ': %s\n' % str(cur_params))
+            sys.stderr.write('Network contains %d total trainable parameters.\n' % n_params)
+            sys.stderr.write('\n')
 
 
 argparser = argparse.ArgumentParser('''
@@ -22,6 +37,9 @@ Trains and evaluates a DNN semproc model from counts file.
 argparser.add_argument('path', help='Path to data file')
 argparser.add_argument('-m', '--mlr', action='store_true', help='Flag indicating fitting of full MLR model (no embedding layer)')
 argparser.add_argument('-c', '--cpuonly', action='store_true', help='Do not use GPU if available')
+argparser.add_argument('-d', '--dev', type=str, default=None, help='Path to dev (cross-validation) data, if used.')
+argparser.add_argument('-o', '--outdir', type=str, default='./semprocDNN_output/', help='Path to output directory.')
+argparser.add_argument('-e', '--embdim', type=int, default=256, help='Embedding dimensions')
 args = argparser.parse_args()
 
 mlr = args.mlr
@@ -32,105 +50,168 @@ import tensorflow as tf
 
 d = Data(args.path)
 X, y, w = d.F.to_matrices()
+print(X.shape)
+print(y.shape)
+print(w.shape)
+if args.dev is not None:
+    X_cv, y_cv, w_cv = d.encode_data(args.dev, 'F')
+    print(X_cv.shape)
+    print(y_cv.shape)
+    print(w_cv.shape)
 
 batch_size = 256
-embedding_dim = 256
 depth = 1
 
 sess = tf.Session()
 with sess.as_default():
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
+    with sess.graph.as_default():
+        X_ = tf.sparse_placeholder(tf.float32, shape=[None, d.F.n_condition])
+        y_ = tf.placeholder(tf.int32, shape=[None])
+        w_ = tf.placeholder(tf.float32, [None])
 
 
-    X_ = tf.sparse_placeholder(tf.float32, shape=[None, d.F.n_condition])
-    y_ = tf.placeholder(tf.int32, shape=[None])
-    w_ = tf.placeholder(tf.float32, [None])
-   
-    if mlr:
-        bias_final = tf.Variable(tf.random_normal([], stddev=0.1), name='bias_final', dtype=tf.float32)
-        weights_final = tf.Variable(tf.random_normal([d.F.n_condition, d.F.n_consequent], stddev=0.1), name='weights_final', dtype=tf.float32)
-        activity_final = bias_final + tf.sparse_tensor_dense_matmul(X_, weights_final)
-        
-    else:
-        embedding_weights = tf.Variable(tf.random_normal([d.F.n_condition, embedding_dim], stddev=0.1), name='embeddings')
-        embedding = tf.sparse_tensor_dense_matmul(X_, embedding_weights)
 
-        layers = [embedding]
+        if mlr:
+            weights_final = tf.Variable(tf.random_normal([d.F.n_condition, d.F.n_consequent], stddev=0.1), name='weights_final', dtype=tf.float32)
+            activity_final = tf.sparse_tensor_dense_matmul(X_, weights_final)
 
-        bias = [None] * depth
-        weights = [None] * depth
-        activity = [None] * depth
-        activation = [None] * depth
+        else:
+            embedding_weights = tf.Variable(tf.random_normal([d.F.n_condition, args.embdim], stddev=0.1), name='embeddings')
+            embedding = tf.sparse_tensor_dense_matmul(X_, embedding_weights)
 
-        for i in range(depth-1):
-            bias[i] = tf.Variable(tf.random_normal([], stddev=0.1), name='bias_%d'%i, dtype=tf.float32)
-            weights[i] = tf.Variable(tf.random_normal([embedding_dim, embedding_dim], stddev=0.1), name='weights_%d'%i, dtype=tf.float32)
-            activity[i] = bias[i] + tf.matmul(embedding, weights[i])
-            activation[i] = tf.nn.relu(activity[i], name='activation_%d'%i)
-            layers.append(activation)
+            layers = [embedding]
 
-        bias_final = tf.Variable(tf.random_normal([], stddev=0.1), name='bias_final', dtype=tf.float32)
-        weights_final = tf.Variable(tf.random_normal([embedding_dim, d.F.n_consequent], stddev=0.1), name='weights_final', dtype=tf.float32)
-        activity_final = bias_final + tf.matmul(layers[-1], weights_final)
+            weights = [None] * depth
+            activity = [None] * depth
+            activation = [None] * depth
 
-    logits = activity_final
-    loss_per_example = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_, logits=logits)
-    weighted_loss_per_example = loss_per_example * w_
-    loss_op = tf.reduce_mean(weighted_loss_per_example)
+            for i in range(depth-1):
+                weights[i] = tf.Variable(tf.random_normal([args.embdim, args.embdim], stddev=0.1), name='weights_%d'%i, dtype=tf.float32)
+                activity[i] = tf.matmul(embedding, weights[i])
+                activation[i] = tf.nn.relu(activity[i], name='activation_%d'%i)
+                layers.append(activation)
 
-    soft_predictions = tf.nn.softmax(logits)
-    hard_predictions = tf.cast(tf.argmax(soft_predictions, axis=1), tf.int32)
-    weighted_accuracy_per_example = tf.cast(tf.equal(y_, hard_predictions), tf.float32) * w_
-    acc_op = tf.reduce_sum(weighted_accuracy_per_example) / tf.reduce_sum(w_)
+            weights_final = tf.Variable(tf.random_normal([args.embdim, d.F.n_consequent], stddev=0.1), name='weights_final', dtype=tf.float32)
+            activity_final = tf.matmul(layers[-1], weights_final)
 
-    global_step = tf.Variable(0, trainable=False)
+        logits = activity_final
+        loss_per_example = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_, logits=logits)
+        weighted_loss_per_example = loss_per_example * w_
+        loss_op = tf.reduce_mean(weighted_loss_per_example)
 
-    opt = tf.contrib.opt.NadamOptimizer()
-    train_op = opt.minimize(
-        loss=loss_op,
-        global_step=global_step
-    )
+        soft_predictions = tf.nn.softmax(logits)
+        hard_predictions = tf.cast(tf.argmax(soft_predictions, axis=1), tf.int32)
+        weighted_accuracy_per_example = tf.cast(tf.equal(y_, hard_predictions), tf.float32) * w_
+        acc_op = tf.reduce_sum(weighted_accuracy_per_example) / tf.reduce_sum(w_)
 
-sess.run(tf.global_variables_initializer())
+        global_step = tf.Variable(0, trainable=False)
+        incr_global_step = tf.assign(global_step, global_step + 1)
+        global_batch_step = tf.Variable(0, trainable=False)
 
-iteration = 0
-n = X.shape[0]
-p = np.arange(n)
-n_batch = math.ceil(n/batch_size)
-
-
-while iteration < 1000:
-    sys.stderr.write('\n' + '='*50 + '\n')
-    sys.stderr.write('Iteration %d\n\n' %(iteration + 1))
-
-    loss = 0
-    acc = 0
-    np.random.shuffle(p)
-
-    pb = tf.contrib.keras.utils.Progbar(n_batch)
-
-    for i in range(0, n, batch_size):
-        batch_len = min(batch_size, n-i)
-        X_coo = X[p[i:i+batch_size]].tocoo()
-        X_batch = tf.SparseTensorValue(
-            indices=np.stack([X_coo.row, X_coo.col], axis=1),
-            values=X_coo.data,
-            dense_shape=X_coo.shape
+        opt = tf.contrib.opt.NadamOptimizer()
+        train_op = opt.minimize(
+            loss=loss_op,
+            global_step=global_batch_step
         )
+        saver = tf.train.Saver()
 
-        y_batch = y[p[i:i+batch_size]]
-        w_batch = w[p[i:i+batch_size]]
+        loss_total_summary = tf.placeholder(shape=[], dtype='float32', name='loss_total')
+        acc_total_summary = tf.placeholder(shape=[], dtype='float32', name='acc_total')
 
-        fd = {
-            X_: X_batch,
-            y_: y_batch,
-            w_: w_batch
-        }
+        tf.summary.scalar('loss', loss_total_summary)
+        tf.summary.scalar('acc', acc_total_summary)
 
-        _, loss_batch, acc_batch = sess.run([train_op, loss_op, acc_op], feed_dict=fd)
-        pb.update((i/batch_size)+1, values=[('loss', loss_batch), ('acc', acc_batch)])
-    
-    iteration += 1
+        summary = tf.summary.merge_all()
+
+        writer_train = tf.summary.FileWriter(args.outdir + '/tensorboard/train')
+        writer_cv = tf.summary.FileWriter(args.outdir + '/tensorboard/cv')
+
+        sess.run(tf.global_variables_initializer())
+
+        if os.path.exists(args.outdir + '/checkpoint'):
+            saver.restore(sess, args.outdir + '/model.ckpt')
+
+
+
+        n = X.shape[0]
+        p = np.arange(n)
+        n_batch = math.ceil(n/batch_size)
+
+        print(global_batch_step.eval(session=sess))
+        report_n_params(sess)
+
+        while global_step.eval(session=sess) < 1000:
+            iteration = global_step.eval(session=sess)
+            sys.stderr.write('\n' + '='*50 + '\n')
+            sys.stderr.write('Iteration %d\n\n' %(iteration + 1))
+
+            loss = 0
+            acc = 0
+            np.random.shuffle(p)
+
+            pb = tf.contrib.keras.utils.Progbar(n_batch)
+
+            loss_total = 0
+            acc_total = 0
+
+            for i in range(0, n, batch_size):
+                batch_len = min(batch_size, n-i)
+                X_coo = X[p[i:i+batch_size]].tocoo()
+                X_batch = tf.SparseTensorValue(
+                    indices=np.stack([X_coo.row, X_coo.col], axis=1),
+                    values=X_coo.data,
+                    dense_shape=X_coo.shape
+                )
+
+                y_batch = y[p[i:i+batch_size]]
+                w_batch = w[p[i:i+batch_size]]
+
+                fd = {
+                    X_: X_batch,
+                    y_: y_batch,
+                    w_: w_batch
+                }
+
+                if args.dev is not None:
+                    X_cv_coo = X_cv.tocoo()
+                    X_cv_batch = tf.SparseTensorValue(
+                        indices=np.stack([X_cv_coo.row, X_cv_coo.col], axis=1),
+                        values=X_cv_coo.data,
+                        dense_shape=X_cv_coo.shape
+                    )
+
+                    fd_cv = {
+                        X_: X_cv_batch,
+                        y_: y_cv,
+                        w_: w_cv
+                    }
+
+                _, loss_batch, acc_batch = sess.run([train_op, loss_op, acc_op], feed_dict=fd)
+                pb.update((i/batch_size)+1, values=[('loss', loss_batch), ('acc', acc_batch)])
+
+                loss_total += loss_batch * X_coo.shape[0]
+                acc_total += acc_batch * X_coo.shape[0]
+
+            loss_total /= n
+            acc_total /= n
+
+            if iteration % 5 == 0:
+                if not args.mlr:
+                    dump_embeddings(args.outdir, args.embdim)
+                    dump_weights(args.outdir, args.embdim)
+            saver.save(sess, args.outdir + '/model.ckpt')
+
+            loss_cv, acc_cv = sess.run([loss_op, acc_op], feed_dict=fd_cv)
+
+            summary_iter_train = sess.run(summary, feed_dict = {loss_total_summary:loss_total, acc_total_summary:acc_total})
+            summary_iter_cv = sess.run(summary, feed_dict = {loss_total_summary:loss_cv, acc_total_summary:acc_cv})
+            writer_train.add_summary(summary_iter_train, global_step.eval(session=sess))
+            writer_cv.add_summary(summary_iter_cv, global_step.eval(session=sess))
+            sess.run(incr_global_step)
+
+        if not args.mlr:
+            sys.stderr.write('Linearizing weight matrices...\n')
+            dump_embeddings(args.outdir, args.embdim)
+            dump_weights(args.outdir, args.embdim)
 
 
